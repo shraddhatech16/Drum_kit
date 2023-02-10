1f18bb259e705b584830f57e3eeb17490a818dbc
shraddhatech16 <rajendraalawane@gmail.com>
2023-02-11 00:07:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+//for button press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButtons;i++)//. used for class
+{
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+// console.log(this);
+//this.style.color="white";
+var buttonInnerHTML = this.innerHTML;
+
+makeSound(buttonInnerHTML);
+    
+});
+}
+//for keyboard press
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    
+});
+
+
+function makeSound(key){
+        switch(key){
+            case "w":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+            break;
+            case "a":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+            break;
+            case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+            break;
+            case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+            break;
+            case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();    
+            break;
+            case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+            break;
+            case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+            break;
+        
+            default:
+        }
+
+}
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//     audio.play();
